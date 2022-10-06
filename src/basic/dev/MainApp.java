dc37865a902e4e6232fd40b6647db07e331f9e5c
VANDINHHOAI <115123521+VANDINHHOAI@users.noreply.github.com>
2022-10-06 10:00:30 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,31 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Thong tin sinh vien
+		
+		String name1 = "Van Dinh Hoai";
+		int msv = 2260690;
+		String cmnd = "VAN DINH HOAI";
+		String className = "Tin hoc ung dung";
+		String school = "Cao dang cong nghiep Hue";
+		double diem = 7.0;
+		
+		//Thong tin cong nhan
+		
+		String name2 = "Van Dinh Hoai";
+		String address = "Thua Thien Hue";
+		double he_so_luong = 2.10;
+		String sdt = "0334840493";
+		
+		//Thong tin mot chiec xe car
+		
+		String nameCar = "Honda Winner v2";
+		String hang = "Honda";
+		int gia = 1500000000;
+		String color = "red";
+		
+	}
+}
